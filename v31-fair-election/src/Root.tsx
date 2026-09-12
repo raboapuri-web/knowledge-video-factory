1e401a95bcb528570966c5370a4eca5ac611b1ad
raboapuri-web <raboapuri@gmail.com>
2026-09-12 20:21:14 +0900

    Add V31 composition

---
+++ v31-fair-election/src/Root.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import {Composition} from 'remotion';
+import sync from './sync-timing.json';
+import {V31FairElection} from './V31FairElection';
+export const RemotionRoot:React.FC=()=>{const durationInFrames=Math.max(30,Math.ceil(Number(sync.durationSeconds||1200)*30));return <Composition id="V31FairElection" component={V31FairElection} durationInFrames={durationInFrames} fps={30} width={1920} height={1080}/>;};
