fe6711c9f920759998004030e1a0f4e85210f587
raboapuri-web <raboapuri@gmail.com>
2026-09-16 09:16:59 +0900

    Fix V48 runtime template interpolation escaping

--- shared/bootstrap/v48-tokyo-envy-runner.mjs
+++ shared/bootstrap/v48-tokyo-envy-runner.mjs
@@ -5,8 +5,8 @@ import {pathToFileURL} from 'node:url';
 const src=path.join(process.cwd(),'shared/bootstrap/v48-tokyo-envy.mjs');
 let code=fs.readFileSync(src,'utf8');
 code=code
-  .replace("width:\\`${50-30*q}%\\`","width:(50-30*q)+'%'")
-  .replace("width:\\`${50+30*q}%\\`","width:(50+30*q)+'%'");
+  .replaceAll('${50-30*q}','\\${50-30*q}')
+  .replaceAll('${50+30*q}','\\${50+30*q}');
 const runtime=path.join(process.cwd(),'shared/bootstrap/.v48-tokyo-envy-runtime.mjs');
 fs.writeFileSync(runtime,code);
 await import(pathToFileURL(runtime).href+`?v=${Date.now()}`);
